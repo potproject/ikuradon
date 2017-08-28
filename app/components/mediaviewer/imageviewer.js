5b7644275bce04c2c74e6cd0942132cef70e62c1
potproject <pptppctt@gmail.com>
2017-08-28 23:49:21 +0900

    fix imageviewer size

--- app/components/mediaviewer/imageviewer.js
+++ app/components/mediaviewer/imageviewer.js
@@ -2,7 +2,8 @@ import React, { PropTypes } from 'react';
 import {
   Image,
   View,
-  StyleSheet
+  StyleSheet,
+  Dimensions
 } from 'react-native';
 
 export default class ImageViewer extends React.Component {
@@ -19,16 +20,18 @@ export default class ImageViewer extends React.Component {
   render() {
     return (
       <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{ uri: this.state.media_attachments[this.state.index].url }} />
+        <Image style={styles.image} resizeMode={'contain'} source={{ uri: this.state.media_attachments[this.state.index].url }} />
       </View>
     );
   }
 }
-
+const window = Dimensions.get('window');
 const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
-    alignItems: 'stretch'
+    alignItems: 'stretch',
+    width: window.width,
+    height: window.height,
   },
   image: {
     flex: 1
