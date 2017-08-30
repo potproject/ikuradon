b283db93d86e587f3c0de8263a3da5100f28a528
potproject <pptppctt@gmail.com>
2017-08-31 00:42:27 +0900

    update sdk

--- App.js
+++ App.js
@@ -1,2 +1,24 @@
-import App from './app/';
-export default App;
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createMiddleware } from "./app/middleware";
+import AppWithNavigationState from './app/navigators/appnavigator';
+
+import reducers from './app/reducers';
+// other imports...
+
+// create store...
+const store = compose(
+  applyMiddleware(...createMiddleware())
+)(createStore)(reducers);
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AppWithNavigationState />
+      </Provider>
+    );
+  }
+}
