a1eedb9a11577333d6f4d1389546d66485a0afcc
potproject <pptppctt@gmail.com>
2017-09-18 01:26:45 +0900

    streaming api support(partical)

--- app/components/maintab/setting.js
+++ app/components/maintab/setting.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View ,Button} from 'react-native';
+import { View, Text, Switch } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import * as StreamingActions from '../../actions/actioncreators/streaming';
 import { connect } from 'react-redux';
@@ -9,23 +9,33 @@ class Setting extends React.Component {
   static navigationOptions = {
     title: "Setting",
     tabBarIcon: ({ focused, tintColor }) => (
-                <FontAwesome name="cogs" size={26} color={tintColor} />
-            )
+      <FontAwesome name="cogs" size={26} color={tintColor} />
+    )
   };
   constructor(props) {
     super(props);
     this.props = props;
+    this.state = {
+      setting: {
+        streaming_api_checked: false,
+      }
+    };
   }
   render() {
     return <View>
-        <Button onPress={()=>this.streamStart()} title="Stream API Test Start" />
-        <Button onPress={()=>this.streamStop()} title="Stream API Test Stop" />
+      <Text>Streaming API</Text>
+      <Switch
+        onValueChange={(streamingChecked) => streamingChecked ? this.streamStart() : this.streamStop()}
+        value={this.state.setting.streaming_api_checked}
+      />
     </View>;
   }
-  streamStart(){
+  streamStart() {
+    this.setState({ setting: { streaming_api_checked: true } });
     this.props.StreamingActions.start();
   }
-  streamStop(){
+  streamStop() {
+    this.setState({ setting: { streaming_api_checked: false } });
     this.props.StreamingActions.stop();
   }
 }
