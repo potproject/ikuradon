9a61400678e28d67ff9ba3c6b7bfce1405563d80
potproject <pptppctt@gmail.com>
2018-03-17 16:38:34 +0900

    html link parser fixed

--- app/util/parser.js
+++ app/util/parser.js
@@ -3,8 +3,11 @@ import Moment from "moment/min/moment-with-locales";
 
 export function bodyFormat(body) {
     console.log(body);
-    //改行
+    //改行 <br />
     let newbody = body.replace(/(<br\s*>|<br\s*\/>)/g, "\n");
+    //URL <a > </a>
+    newbody = newbody.replace(/(<a("[^"]*"|'[^']*'|[^'">])*>|<\/a>)/g, " ");
+    //Other HTML Tag
     newbody = newbody.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "");
     return He.unescape(newbody);
 }
