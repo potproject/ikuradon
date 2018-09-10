675c809622f016262d8423b30bd7f6759d5e83e5
potproject <pptppctt@gmail.com>
2018-09-11 00:42:46 +0900

    change tab color

--- app/components/main.js
+++ app/components/main.js
@@ -7,78 +7,45 @@ import FederalTabScreen from "../components/maintab/federal";
 import NotificationsTabScreen from "../components/maintab/notifications";
 import SettingTabScreen from "../components/maintab/setting";
 
-import StreamingButton from "./maintab/streamingbutton";
-import TootButton from "./maintab/tootbutton";
-
 import { FontAwesome } from "@expo/vector-icons";
 import I18n from "../i18n";
 import NotificationsIconBadge from "./maintab/notificationsiconbadge";
 
 const TabNavigator = createBottomTabNavigator(
     {
-        home: {
-            screen: HomeTabScreen,
-            navigationOptions: () => ({
-                title: I18n.t("navigation_home"),
-                tabBarIcon: ({ tintColor }) => (
-                    <FontAwesome name="home" size={32} color={tintColor} />
-                )
-            }),
+        home: HomeTabScreen,
+        local: LocalTabScreen,
+        federal: FederalTabScreen,
+        notifications: NotificationsTabScreen,
+        setting: SettingTabScreen
+    },
+    {
+        navigationOptions: ({ navigation }) => 
+        {
+            return {
+                title: I18n.t(`navigation_${navigation.state.routeName}`),
+                tabBarIcon: ({ tintColor }) => {
+                    const { routeName } = navigation.state;
+                    switch (routeName){
+                        case "home":
+                            return <FontAwesome name="home" size={32} color={tintColor} />;
+                        case "local":
+                            return <FontAwesome name="users" size={26} color={tintColor} />;
+                        case "federal":
+                            return <FontAwesome name="globe" size={32} color={tintColor} />;
+                        case "notifications":
+                            return <NotificationsIconBadge color={tintColor} count={0} />;
+                        case "setting":
+                            return <FontAwesome name="cogs" size={26} color={tintColor} />;
+                    }
+                },
+            };
         },
-        local: {
-            screen: LocalTabScreen,
-            navigationOptions: () => ({
-                title: I18n.t("navigation_local"),
-                tabBarIcon: ({ tintColor }) => (
-                    <FontAwesome name="users" size={26} color={tintColor} />
-                )
-            }),
+        tabBarOptions: {
+            activeTintColor: "tomato",
+            inactiveTintColor: "gray",
         },
-        federal: {
-            screen: FederalTabScreen,
-            navigationOptions: () => ({
-                title: I18n.t("navigation_federal"),
-                tabBarIcon: ({ tintColor }) => (
-                    <FontAwesome name="globe" size={32} color={tintColor} />
-                )
-            }),
-        },
-        notifications: {
-            screen: NotificationsTabScreen,
-            navigationOptions: () => ({
-                title: I18n.t("navigation_notifications"),
-                tabBarIcon: ({ tintColor }) => (
-                    <NotificationsIconBadge color={tintColor} count={0} />
-                )
-            }),
-        },
-        setting: {
-            screen: SettingTabScreen,
-            navigationOptions: () => ({
-                title: I18n.t("navigation_setting"),
-                tabBarIcon: ({ tintColor }) => (
-                    <FontAwesome name="cogs" size={26} color={tintColor} />
-                )
-            }),
-        }
     }
 );
 
-TabNavigator.navigationOptions = ({ navigation }) => {
-    let { routeName } = navigation.state.routes[navigation.state.index];
-    let headerTitle = I18n.t(`navigation_${routeName}`);
-    let headerLeft = routeName === "home" ? <StreamingButton type={routeName} /> : null;
-    let headerRight = <TootButton />;
-    return {
-        headerTitle,
-        headerLeft,
-        headerRight,
-    };
-};
-
 export default TabNavigator;
-/*
-const styles = StyleSheet.create({
-    container: {
-    },
-});*/
--- app/navigators/appnavigator.js
+++ app/navigators/appnavigator.js
@@ -1,9 +1,6 @@
-import {
-    connect
-} from "react-redux";
-import {
-    createStackNavigator
-} from "react-navigation";
+import React from "react";
+import { connect } from "react-redux";
+import { createStackNavigator } from "react-navigation";
 import { reduxifyNavigator } from "react-navigation-redux-helpers";
 
 import { createMiddleware } from "../middleware";
@@ -14,6 +11,9 @@ import MainScreen from "../components/main";
 import LoginScreen from "../components/login";
 import TootScreen from "../components/toot";
 import MediaViewerScreen from "../components/mediaviewer/mediaviewer";
+
+import StreamingButton from "../components/maintab/streamingbutton";
+import TootButton from "../components/maintab/tootbutton";
 
 import I18n from "../i18n";
 import * as RouterName from "../constants/routername";
@@ -29,6 +29,14 @@ export const AppNavigator = createStackNavigator({
 },{
     navigationOptions: ({ navigation })=>{
         let routeName = navigation.state.routeName;
+        if(RouterName.Main === navigation.state.routeName){
+            return {
+                headerTitle: I18n.t(`navigation_${navigation.state.routes[navigation.state.index].routeName}`),
+                headerLeft: navigation.state.routes[navigation.state.index].routeName === "home" ? <StreamingButton type={navigation.state.routes[navigation.state.index].routeName} /> : null,
+                headerRight:  <TootButton />,
+            };
+        }
+
         return {
             title: I18n.t(`${routeName}_title`)
         };
