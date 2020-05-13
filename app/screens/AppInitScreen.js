ebb2cd38830f4da4c1455fcee9fcd8cecd7d6b72
potproject <pptppctt@gmail.com>
2020-05-13 10:57:13 +0900

    fix init

--- app/screens/AppInitScreen.js
+++ app/screens/AppInitScreen.js
@@ -1,8 +1,7 @@
 import * as React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import { Text, Image, StyleSheet, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
-import { Button } from "react-native-elements";
-import * as RouterName from "../constants/RouterName";
+import Constants from "expo-constants";
 
 import { appInit } from "../actions/actioncreators/appinit";
 const AppInitReducerSelector = state => state.appInitReducer;
@@ -17,10 +16,10 @@ function AppInitScreen({ navigation }) {
     return (
         <View style={styles.container}>
             <Image style={styles.logo} source={require("../../assets/image/icon250.png")} />
-            <Button
-                title="Go to Details"
-                onPress={() => {navigation.navigate(RouterName.Login)}}
-            />
+            <View>
+                <Text style={styles.text}>Loading...</Text>
+                <Text style={styles.text}>Version: { Constants.manifest.version }</Text>
+            </View>
         </View>
     );
 }
@@ -30,8 +29,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#FF3300"
     },
-
+    text: {
+        color: "#FFFFFF"
+    },
     logo: {
         width: 100,
         height: 100,
