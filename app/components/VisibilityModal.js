201338b6da6291e3aaaab1b7dbcf49b2764aec31
potproject <pptppctt@gmail.com>
2020-05-14 15:49:33 +0900

    visibility

---
+++ app/components/VisibilityModal.js
@@ -0,0 +1,64 @@
+import React, {useContext} from "react";
+import { View, StyleSheet } from "react-native";
+import { ListItem, ThemeContext } from "react-native-elements";
+import { FontAwesome } from "@expo/vector-icons";
+import t from "../services/I18n";
+
+const list = [
+    {
+        key: "public",
+        icon: "globe",
+        name: t("toot_visibility_public"),
+        subtitle: t("toot_visibility_public_detail")
+    },
+    {
+        key: "unlisted",
+        icon: "unlock",
+        name: t("toot_visibility_unlisted"),
+        subtitle: t("toot_visibility_unlisted_detail")
+    },
+    {
+        key: "private",
+        icon: "lock",
+        name: t("toot_visibility_private"),
+        subtitle: t("toot_visibility_private_detail")
+    },
+    {
+        key: "direct",
+        icon: "envelope",
+        name: t("toot_visibility_direct"),
+        subtitle: t("toot_visibility_direct_detail")
+    }
+];
+
+  
+export default function VisibilityModal({onSelect}){
+    const { theme }= useContext(ThemeContext);
+    return(
+        <View style={styles.container}>
+            {
+                list.map((l, i) => (
+                    <ListItem
+                        key={i}
+                        leftAvatar={() => <FontAwesome style={styles.icon} name={l.icon} size={26} color={theme.colors.grey1} />}
+                        title={l.name}
+                        subtitle={l.subtitle}
+                        bottomDivider={i < 3}
+                        onPress={() => onSelect(l.key)}
+                    />
+                ))
+            }
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        width: 300,
+        height: 260
+    },
+    icon: {
+        width:30,
+        height:30,
+    }
+});
--- app/screens/TootScreen.js
+++ app/screens/TootScreen.js
@@ -1,15 +1,16 @@
 
 import React, { useState, useContext } from "react";
-import { Text, StyleSheet, View, Button, TextInput } from "react-native";
+import { Text, StyleSheet, View, Button, TextInput, TouchableOpacity } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import TimelineLeftHeader from "../components/TimelineLeftHeader";
 import TimelineCenterHeader from "../components/TimelineCenterHeader";
 import TimelineTootButton from "../components/TimelineTootButton";
+import { FontAwesome } from "@expo/vector-icons";
 import KeyboardSpacer from "react-native-keyboard-spacer";
 import t from "../services/I18n";
 
 import { toot as TootAction } from "../actions/actioncreators/toot";
-import { Header, ThemeContext } from "react-native-elements";
+import { Header, Overlay, ThemeContext } from "react-native-elements";
 
 const reducerSelector = state => ({
     current: state.currentUserReducer,
@@ -17,18 +18,30 @@ const reducerSelector = state => ({
 });
 
 import * as RouterName from "../constants/RouterName";
+import VisibilityModal from "../components/VisibilityModal";
 
 const MAX_TOOT_LENGTH = 500;
 const MAX_TOOT_WARNING = MAX_TOOT_LENGTH / 20;
+
+const VISIBILITY_DEFAULT = "public";
+
+const VISIBILITY_CONST = {
+    public: "globe",
+    unlisted: "unlock",
+    private: "lock",
+    direct: "envelope"
+};
 
 function TootScreen({ navigation }) {
     const dispatch = useDispatch();
     const type = RouterName.Toot;
     const { current, toot } = useSelector(reducerSelector);
     const { theme }= useContext(ThemeContext);
+    const [tootText, onChangeTootText] = useState("");
     const [cwTootText, onChangeCwTootText] = useState("");
     const [cw, useCw] = useState(false);
-    const [tootText, onChangeTootText] = useState("");
+    const [visibilityModal, useVisibilityModal] = useState(false);
+    const [visibility, useVisibility] = useState(VISIBILITY_DEFAULT);
     return (
         <View style={styles.container}>
             <Header
@@ -36,7 +49,7 @@ function TootScreen({ navigation }) {
                 centerComponent={<TimelineCenterHeader fixedTitle={false} onPress={navigation.openDrawer} current={current}/>}
                 rightComponent={(
                     <TimelineTootButton
-                        onPress={() => dispatch(TootAction(tootText, "public", cw, cwTootText,[], null, null))}
+                        onPress={() => dispatch(TootAction(tootText, visibility, cw, cwTootText,[], null, null))}
                         enabled={MAX_TOOT_LENGTH - tootText.length - cwTootText.length >= 0}
                         loading={toot.tootWaiting}
                     />
@@ -65,7 +78,13 @@ function TootScreen({ navigation }) {
                     />
                 </View>
                 <View style={[{backgroundColor: theme.colors.charBackground},styles.itemForm]}>
+                    <TouchableOpacity
+                        style={styles.icon}
+                        onPress={() => useVisibilityModal(true)}>
+                        <FontAwesome name={VISIBILITY_CONST[visibility]} size={26} color={theme.colors.grey1} />
+                    </TouchableOpacity>
                     <Button
+                        style={styles.icon}
                         color={cw ? theme.colors.primary : theme.colors.grey0}
                         onPress={() => onChangeCwTootText("") || useCw(!cw)}
                         title="CW"
@@ -74,6 +93,12 @@ function TootScreen({ navigation }) {
                         {MAX_TOOT_LENGTH - tootText.length - cwTootText.length}
                     </Text>
                 </View>
+                <Overlay isVisible={visibilityModal} onBackdropPress={() => useVisibilityModal(false)}>
+                    <VisibilityModal onSelect={(selected)=>{
+                        useVisibilityModal(false);
+                        useVisibility(selected);
+                    }} />
+                </Overlay>
             </View>
             <KeyboardSpacer/>
         </View>
@@ -110,7 +135,14 @@ const styles = StyleSheet.create({
         flexDirection: "row",
     },
     item: {
-        flex: 1
+        flex: 1,
+    },
+    icon: {
+        paddingTop: 5,
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingBottom: 5, 
+        
     },
     countText: {
         fontSize: 20,
