00a3c08fdeca635583a6cc683ee1f772e0410908
potproject <pptppctt@gmail.com>
2020-06-22 21:49:14 +0900

    poll wip

--- app/components/MastoRowPoll.js
+++ app/components/MastoRowPoll.js
@@ -1,21 +1,54 @@
-import React, { memo } from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
+import { ThemeContext, Card, Button } from "react-native-elements";
 import PropTypes from "prop-types";
-import {open as openUrl} from "../util/url";
+import DayJS from "dayjs";
 import t from "../services/I18n";
 
 function MastoRowPoll({poll}){
+    const { theme } = useContext(ThemeContext);
     let {id, expires_at, expired, multiple, votes_count, voters_count, voted, own_votes, options, emojis} = poll;
     return (
-        <View style={[]}>
+        <View style={styles.container}>
             {
-                options.map((v)=>{
-                    return <Text>{v.title+": "+v.votes_count+t("polls.votes")}</Text>
+                options.map((v, i)=>{
+                    return (
+                    <Text style={styles.pollDetailsRow}key={i}>{v.title} - {v.votes_count}{t("polls.votes")} ({votePer(v.votes_count,votes_count)}%)</Text>
+                    );
                 })
             }
+            <Text style={[{color: theme.colors.grey0},styles.pollDetailsRow]}>{voters_count}人 - {timeStr(expires_at, expired)}</Text>
         </View>
     );
 }
+
+function timeStr(expires_at, expired){
+    if(expired){
+        return "終了";
+    }
+    return DayJS(expires_at).diff(DayJS(), "h") + "時間";
+}
+function votePer(count, total){
+    if(count === 0 || total === 0){
+        return 0;
+    }
+    return Math.floor(count / total * 100);
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    pollDetailsRow:{
+        flex: 1,
+        marginTop: 5,
+        marginLeft: 10,
+        marginRight:10,
+        fontSize: 14,
+    },
+});
 
 MastoRowPoll.propTypes = {
     poll: PropTypes.shape(
@@ -36,8 +69,9 @@ MastoRowPoll.propTypes = {
                     }
                 )
             ),
-            emojis: PropTypes.object,
+            emojis: PropTypes.array,
         }
     )
-}; 
+};
+
 export default MastoRowPoll;
