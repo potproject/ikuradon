26a76018555dc880139c4bb0b496a5012b2bc9c1
potproject <pptppctt@gmail.com>
2020-09-02 00:19:31 +0900

    add test

---
+++ app/util/__tests__/storage-test.js
@@ -0,0 +1,24 @@
+import { getItem, setItem, removeItem } from "../storage";
+import AsyncStorage from "@react-native-community/async-storage";
+
+describe("Util/Storage", () => {
+    it("getItem", async () => {
+        AsyncStorage.getItem.mockImplementation(() => "{\"test\":1}");
+        expect(await getItem("test")).toEqual({ "test": 1 });
+        AsyncStorage.getItem.mockClear();
+    });
+    it("setItem", async done => {
+        AsyncStorage.setItem.mockImplementation((k, v) => {
+            if (k === "test" && v === "{\"test\":1}"){
+                done();
+            }
+        });
+        await setItem("test", { "test": 1 });
+        AsyncStorage.getItem.mockClear();
+    });
+    it("removeItem", async done => {
+        AsyncStorage.removeItem.mockImplementation((k) => k === "test" && done());
+        await removeItem("test");
+        AsyncStorage.removeItem.mockClear();
+    });
+});
