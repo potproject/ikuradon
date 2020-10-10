cbb12716d6b96383c8ef6538ce59a1ccaa7f5de9
potproject <pptppctt@gmail.com>
2020-10-10 21:58:17 +0900

    nav

--- app/actions/actioncreators/detail.js
+++ app/actions/actioncreators/detail.js
@@ -11,7 +11,8 @@ import DropDownHolder from "../../services/DropDownHolder";
 export function openDetail(id) {
     console.log(id);
     return async dispatch => {
-        try {  
+        try {
+            NavigationService.navigate({ name: RouterName.Detail });
             dispatch({ type: Detail.DETAIL_OPEN });
         } catch (e) {
             DropDownHolder.error(t("Errors_error"), e.message);
