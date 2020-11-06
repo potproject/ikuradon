d605b78246cd8e331974f4511c253c8e7fc97165
potproject <pptppctt@gmail.com>
2020-11-06 11:13:33 +0900

    yoshi!

--- app/components/OpenSticker.js
+++ app/components/OpenSticker.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import { Image } from "react-native-elements";
 
 export default function OpenSticker({ acct, currentDomain, data }){
     const sticker = getSticker(acct, currentDomain, data);
@@ -7,8 +8,16 @@ export default function OpenSticker({ acct, currentDomain, data }){
         return null;
     }
     return (
-        <View style={styles.design}>
-            <Text>{sticker.domain}</Text>
+        <View style={[styles.innerContainer, {
+            backgroundColor: sticker.bgColor,
+        }]}>
+            <View style={styles.padding} ></View>
+            <Text style={[styles.sticker, { color:sticker.fontColor }]} ellipsizeMode="tail" numberOfLines={1}>
+                <Image
+                    source={{ uri: sticker.favicon }}
+                    style={styles.photo}/>
+                {sticker.name}
+            </Text>
         </View>
     );
 }
@@ -24,7 +33,26 @@ function getSticker(acct, currentDomain, data){
     return data[domain];
 }
 const styles = StyleSheet.create({
-    design: {
-
-    }
+    innerContainer: {
+        flex: 1,
+        flexDirection: "row",
+        borderWidth: 0,
+        height: "auto",
+        marginTop:1,
+        marginBottom:2
+    },
+    padding:{
+        flex: 0.18,
+        flexDirection: "column",
+    },
+    sticker: {
+        flex: 0.82,
+        flexDirection: "column",
+        fontSize: 15
+    },
+    photo: {
+        width: 15,
+        height: 15,
+        marginRight:2
+    },
 });
