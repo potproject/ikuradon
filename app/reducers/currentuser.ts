2034ab6eb9b3f7f51ee2a8ebd400aa9c21af1f8f
potproject <pptppctt@gmail.com>
2022-05-19 21:29:34 +0900

    fi

--- app/reducers/currentuser.ts
+++ app/reducers/currentuser.ts
@@ -1,7 +1,15 @@
 import * as CurrentUserActionTypes from "../actions/actiontypes/currentuser";
 import { createReducer } from "@reduxjs/toolkit";
 
-export const initialState = {
+type currentUser = {
+    user_credentials: any;
+    domain: string;
+    access_token: string;
+    notification_count: number;
+    instance: any;
+};
+
+export const initialState: currentUser = {
     user_credentials: null,
     domain: "",
     access_token: "",
