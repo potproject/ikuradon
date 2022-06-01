fd03eee045a4765ff59b02c21553d0c20d364c28
potproject <pptppctt@gmail.com>
2022-06-01 23:59:37 +0900

    reaction ready

--- app/constants/api.ts
+++ app/constants/api.ts
@@ -1,28 +1,58 @@
-export const UPLOAD_POST_MEDIA_V1 = {
+export type api = {
+    method: "post"|"get";
+    url: string;
+    form: {};
+};
+
+type streaming = {
+    url: string;
+}
+
+export const UPLOAD_POST_MEDIA_V1: api = {
     method: "post",
     url: "/api/v1/media",
     form: {}
 };
 
-export const UPLOAD_POST_MEDIA_V2 = {
+export const UPLOAD_POST_MEDIA_V2: api = {
     method: "post",
     url: "/api/v2/media",
     form: {}
 };
 
-export const UPLOAD_POST_MEDIA_MISSKEY = {
+export const UPLOAD_POST_MEDIA_MISSKEY: api = {
     method: "post",
     url: "/api/drive/files/create",
     form: {}
 };
 
-export const UPLOAD_GET_MEDIA = {
+export const UPLOAD_GET_MEDIA: api = {
     method: "get",
     url: "/api/v1/media/:param:",
     form: {}
 };
 
+
+export const MISSKEY_CREATE_REACTION: api = {
+    method: "post",
+    url: "/api/reactions/create",
+    form: {
+        i: "", //required
+        noteId: "", //required
+        reaction: "", //required
+    }
+};
+
+export const MISSKEY_DELETE_REACTION: api = {
+    method: "post",
+    url: "/api/reactions/delete",
+    form: {
+        i: "", //required
+        noteId: "", //required
+    }
+};
+
 /** Streaming API とりあえずUSERのみサポート */
-export const STREAMING = {
+export const STREAMING: streaming = {
     url: "/api/v1/streaming"
 };
