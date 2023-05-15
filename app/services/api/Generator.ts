d39acf7d0efb670ebf1b0a6a87a9c5cb18ba8172
potproject <pptppctt@gmail.com>
2023-05-15 16:59:58 +0900

    fix Generator & Reaction

--- app/services/api/Generator.ts
+++ app/services/api/Generator.ts
@@ -3,7 +3,7 @@ import blueSkyGenerator from "./Bluesky/Generetor";
 
 export default function generatorBase(sns: "mastodon"|"pleroma"|"misskey"|"bluesky", baseUrl: string, access_token: string = "") {
     if (sns === "bluesky") {
-        const bsky = new blueSkyGenerator(baseUrl, access_token);
+        const bsky = new blueSkyGenerator(baseUrl, access_token || "{}");
         return bsky;
     }
     return generator(sns, baseUrl, access_token);
